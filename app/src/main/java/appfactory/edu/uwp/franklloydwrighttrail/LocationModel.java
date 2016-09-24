0578ca0151bc308e6c75b12e86ce0b858d99465c
sterlingaceto <sterlingaceto@hotmail.com>
2016-09-24 17:46:01 -0500

    New API Key and ready for merge.

--- app/src/main/java/appfactory/edu/uwp/franklloydwrighttrail/LocationModel.java
+++ app/src/main/java/appfactory/edu/uwp/franklloydwrighttrail/LocationModel.java
@@ -8,12 +8,12 @@ public class LocationModel {
     @NonNull
     public static ArrayList<FLWLocation> getLocations() {
         ArrayList<FLWLocation> locations = new ArrayList<>();
-        locations.add(0, new FLWLocation(R.string.scjohnson, R.mipmap.scjohnson, R.drawable.marker_red));
-        locations.add(1, new FLWLocation(R.string.monona_terrace, R.mipmap.monona_terrace, R.drawable.marker_orange));
-        locations.add(2, new FLWLocation(R.string.wingspread, R.mipmap.wingspread, R.drawable.marker_yellow));
-        locations.add(3, new FLWLocation(R.string.meeting_house, R.mipmap.meeting_house, R.drawable.marker_green));
-        locations.add(4, new FLWLocation(R.string.visitor_center, R.mipmap.visitor_center, R.drawable.marker_blue));
-        locations.add(5, new FLWLocation(R.string.german_warehouse, R.mipmap.german_warehouse, R.drawable.marker_violet));
+        locations.add(0, new FLWLocation(R.string.scjohnson, R.drawable.scjohnson, R.drawable.marker_red));
+        locations.add(1, new FLWLocation(R.string.monona_terrace, R.drawable.monona_terrace, R.drawable.marker_orange));
+        locations.add(2, new FLWLocation(R.string.wingspread, R.drawable.wingspread, R.drawable.marker_yellow));
+        locations.add(3, new FLWLocation(R.string.meeting_house, R.drawable.meeting_house, R.drawable.marker_green));
+        locations.add(4, new FLWLocation(R.string.visitor_center, R.drawable.visitor_center, R.drawable.marker_blue));
+        locations.add(5, new FLWLocation(R.string.german_warehouse, R.drawable.german_warehouse, R.drawable.marker_violet));
 
         return locations;
     }
