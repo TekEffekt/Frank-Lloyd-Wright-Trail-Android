6aac419a81a9ffe355a8874c164c45ce319d6207
Sterling Aceto <aceto013@rangers.uwp.edu>
2017-01-08 01:31:03 -0600

    Removed Nav-Bar from Trip planner building.

--- app/src/main/java/appfactory/edu/uwp/franklloydwrighttrail/TripPlannerOptions.java
+++ app/src/main/java/appfactory/edu/uwp/franklloydwrighttrail/TripPlannerOptions.java
@@ -3,14 +3,8 @@ package appfactory.edu.uwp.franklloydwrighttrail;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -24,8 +18,7 @@ import io.realm.Realm;
  * Created by sterl on 10/28/2016.
  */
 
-public class TripPlannerOptions extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private DrawerLayout drawer;
+public class TripPlannerOptions extends AppCompatActivity{
     private Button cont;
 
     private Switch breakfastSwitch;
@@ -61,15 +54,6 @@ public class TripPlannerOptions extends AppCompatActivity implements NavigationV
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.trip_options);
         setSupportActionBar(toolbar);
-
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
         cont = (Button) findViewById(R.id.cont);
         initializeViews();
 
@@ -131,15 +115,6 @@ public class TripPlannerOptions extends AppCompatActivity implements NavigationV
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-                super.onBackPressed();
-        }
-    }
-
     /*
         Switches the appr
      */
@@ -151,38 +126,6 @@ public class TripPlannerOptions extends AppCompatActivity implements NavigationV
             mealContainer.setVisibility(View.VISIBLE);
             return true;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_location_selection, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return false;
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_locations:
-                break;
-            case R.id.nav_trip_planner:
-                break;
-            //case R.id.nav_scrapbook:
-            //    break;
-            case R.id.nav_settings:
-                break;
-            case R.id.nav_about:
-                break;
-        }
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     // Easy Clump of all the used views
