8db545b89d6699ce304086ab4523dc45df789862
Sterling Aceto <aceto013@rangers.uwp.edu>
2017-02-19 18:47:29 -0600

    Current progress on tour time view

---
+++ app/src/main/java/appfactory/edu/uwp/franklloydwrighttrail/Fragments/TripPlannerTourTimesFragment.java
@@ -0,0 +1,24 @@
+package appfactory.edu.uwp.franklloydwrighttrail.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sterl on 2/19/2017.
+ */
+
+public class TripPlannerTourTimesFragment extends Fragment {
+
+    public static TripPlannerTourTimesFragment newInstance(){
+        TripPlannerTourTimesFragment tripPlannerTourTimesFragment = new TripPlannerTourTimesFragment();
+        return tripPlannerTourTimesFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_trip_tour_times, container, false);
+    }
+}
